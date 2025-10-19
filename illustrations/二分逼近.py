0732f149483975b336d7a5eec8926b4e708c5fa0
minstrike520 <minstrike520@gmail.com>
2025-10-19 22:37:32 +0800

    remove temp files

--- illustrations/二分逼近.py
+++
@@ -1,9 +0,0 @@
-low = 0
-high = 2  # √2 一定在 0 到 2 之間
-for i in range(100):
-    mid = (low + high) / 2
-    if mid * mid < 2:
-        low = mid
-    else:
-        high = mid
-print((low + high) / 2)
