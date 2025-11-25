69664a1b9382ad6d807a9ba4d4c75ed39416bb01
minstrike520 <minstrike520@gmail.com>
2025-11-25 20:24:11 +0800

    refactor: adds ParseResult

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -10,6 +10,20 @@ pub struct Parser {
     tokens: VecDeque<Token>,
 }
 
+#[derive(Debug, Clone)]
+pub enum ParseError {
+    SyntaxError(String),
+    TypeError(String),
+}
+
+type ParseResult<T> = Result<T, ParseError>;
+
+macro_rules! return_syntax_err {
+    ($content:expr) => {
+        return Err(ParseError::SyntaxError($content.to_string()))
+    };
+}
+
 impl Parser {
     pub fn initialize(source_code: String) -> Self {
         Self {
@@ -28,83 +42,93 @@ impl Parser {
     fn pop_front(&mut self) -> Token {
         self.tokens.pop_front().unwrap()
     }
-    fn parse_additive_expression(&mut self) -> Expression {
-        let mut left = self.parse_multiplicitave_expression();
+    fn parse_additive_expression(&mut self) -> ParseResult<Expression> {
+        let mut left = self.parse_multiplicitave_expression()?;
 
         while matches!(
             self.at(),
             Token::BinaryOperator(BinaryOperator::Additive(_))
         ) {
             let operator = self.pop_front().to_string();
-            let right = self.parse_multiplicitave_expression();
+            let right = self.parse_multiplicitave_expression()?;
             left = Expression::BinaryExpression {
                 left: Box::new(left),
                 right: Box::new(right),
                 operator,
             }
         }
-        left
+        Ok(left)
     }
-    fn parse_multiplicitave_expression(&mut self) -> Expression {
-        let mut left = self.parse_primary_expression();
+    fn parse_multiplicitave_expression(&mut self) -> ParseResult<Expression> {
+        let mut left = self.parse_primary_expression()?;
         while matches!(
             self.at(),
             Token::BinaryOperator(BinaryOperator::Multiplicitave(_))
         ) {
             let operator = self.pop_front().to_string();
-            let right = self.parse_primary_expression();
+            let right = self.parse_primary_expression()?;
             left = Expression::BinaryExpression {
                 left: Box::new(left),
                 right: Box::new(right),
                 operator,
             }
         }
-        left
+        Ok(left)
     }
-    fn parse_primary_expression(&mut self) -> Expression {
+    fn parse_primary_expression(&mut self) -> ParseResult<Expression> {
         let token = self.pop_front();
-        match token {
+        Ok(match token {
             Token::Identifier(value) => Expression::Identifier(value),
             Token::Number(value) => Expression::NumericLiteral(value.parse::<i32>().unwrap()),
             Token::OpenParen => {
                 let expr = self.parse_expression();
-                assert!(
-                    self.pop_front() == Token::CloseParen,
-                    "syntax error: Expected a close parenthesis"
-                );
+                if self.pop_front() != Token::CloseParen {
+                    return_syntax_err!("expected a close parenthesis");
+                }
                 expr
             }
             Token::CloseParen => {
-                panic!("syntax error: A close parenthesis is missing a corresponding open one.")
+                return_syntax_err!("'(' was never closed");
             }
-            _ => panic!("Not an expression token: {token}"),
-        }
+            _ => {
+                return_syntax_err!(format!("'{token}' is not an expression token"));
+            }
+        })
     }
-    fn parse_let(&mut self) -> Statement {
+    fn parse_let(&mut self) -> ParseResult<Statement> {
         assert!(self.pop_front() == Token::Let);
         let identifier = match self.pop_front() {
             Token::Identifier(i) => i,
-            _ => panic!("syntax error: constant statement should be followed by an identifier."),
+            _ => {
+                return_syntax_err!("constant statement should be followed by an identifier")
+            }
         };
-        match self.pop_front() {
-            Token::Semicolon => {
-                Statement::VarDeclaration { identifier, value: None }
+        Ok(match self.pop_front() {
+            Token::Semicolon => Statement::VarDeclaration {
+                identifier,
+                value: None,
             },
             Token::Equals => {
                 let value = Some(self.parse_expression());
                 if self.pop_front() != Token::Semicolon {
-                    panic!("syntax error: Constant declaration statement must end with semicolon.")
+                    return_syntax_err!("constant declaration statement must end with semicolon");
                 }
                 Statement::VarDeclaration { identifier, value }
-            },
-            t => panic!("syntax error: Not a valid constant assignment (expecting '=' or ';', but '{}' found)", t.to_string())
-        }
+            }
+            t => {
+                return_syntax_err!(format!(
+                    "not a valid constant assignment (expecting '=' or ';', but '{t}' found)"
+                ));
+            }
+        })
     }
-    fn parse_const(&mut self) -> Statement {
+    fn parse_const(&mut self) -> ParseResult<Statement> {
         assert!(self.pop_front() == Token::Const);
         let identifier = match self.pop_front() {
             Token::Identifier(i) => i,
-            _ => panic!("syntax error: let statement should be followed by an identifier."),
+            _ => {
+                return_syntax_err!("let statement should be followed by an identifier");
+            } //panic!("syntax error: let statement should be followed by an identifier."),
         };
         match self.pop_front() {
             Token::Semicolon => {
@@ -113,14 +137,14 @@ impl Parser {
             Token::Equals => {
                 let value = self.parse_expression();
                 if self.pop_front() != Token::Semicolon {
-                    panic!("syntax error: Variable declaration statement must end with semicolon.")
+                    return_syntax_err!("variable declaration statement must end with semicolon.");
                 }
                 Statement::ConstDeclaration { identifier, value }
             }
-            t => panic!(
-                "syntax error: Not a valid let assignment (expecting '=' or ';', but '{}' found)",
+            t => return_syntax_err!(format!(
+                "not a valid let assignment (expecting '=' or ';', but '{}' found)",
                 t.to_string()
-            ),
+            )),
         }
     }
     fn parse_identifier(&mut self) -> Statement {
@@ -136,15 +160,10 @@ impl Parser {
         }
         assert!(matches!(left, Expression::Identifier(_)));
         if let Expression::Identifier(identifier) = left {
-            Statement::VarAssignment {
-                identifier,
-                value
-            }
-        }
-        else {
+            Statement::VarAssignment { identifier, value }
+        } else {
             panic!()
         }
-        
     }
     fn parse_expression(&mut self) -> Expression {
         self.parse_additive_expression()
